faf09eff1b1bda2229749d1d35e4e5f60a98222c
hasan <hassanhitleap1@gmail.com>
2018-07-12 15:41:12 +0300

    home blade

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,18 +3,35 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-8">
+        <div class="col-md-6">
             <div class="card">
-                <div class="card-header">Dashboard</div>
-
+                <div class="card-header">from</div>
                 <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Select the way ">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-header">To</div>
+                <div class="card-body">
+                    <div class="input-group">
+                        <input type="text" class="form-control" placeholder="Select the way ">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-md-12">
+            <div class="card">
+                <div class="card-header"> </div>
+                <div class="card-body">
+                    <div class="input-group">
+                        <button  class="btn btn-success btn-lg btn-block" > sbmint</button>
+                    </div>
                 </div>
             </div>
         </div>
